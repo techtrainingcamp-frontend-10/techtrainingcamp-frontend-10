e750e44fe1b121869b801672d32853700020936f
fly3949 <i@fly.moe>
2021-01-26 18:10:16 +0800

    feat: 生产环境启用 mock

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -2,9 +2,9 @@ import http from 'axios'
 
 http.defaults.baseURL = process.env.REACT_APP_API
 
-if (process.env.NODE_ENV === 'development') {
-  require('../../mock/api')
-}
+// if (process.env.NODE_ENV === 'development') {
+require('../../mock/api')
+// }
 
 // 请求拦截器
 http.interceptors.request.use(function (config) {
